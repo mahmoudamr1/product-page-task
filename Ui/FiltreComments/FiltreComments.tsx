15366eca86deca4cec6f903b2868e0ceebd49016
mahmoud amr <mahmoudamr700@gmail.com>
2025-06-23 13:40:49 +0300

    update

--- Ui/FiltreComments/FiltreComments.tsx
+++ Ui/FiltreComments/FiltreComments.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./FiltreComments.css";
 import DashedLine from "@/components/DashedLine/DashedLine";
+import SoldRating from "../SoldRating/SoldRating";
 
 interface FiltreCommentsProps {
   className?: string;
@@ -9,16 +10,111 @@ interface FiltreCommentsProps {
 const FiltreComments: React.FC<FiltreCommentsProps> = ({ className }) => {
   return (
     <div className={className}>
-      <div className="ReviewsFilter-container flex flex-col gap-3 gap-lg-4 p-4">
+      <div className="ReviewsFilter-container flex flex-col gap-3 gap-lg-4 p-6 ">
         <div className="ReviewsFilter">Reviews Filter</div>
         <DashedLine className="!py-1" />
         <div className="Rating">Rating</div>
-        <div className="all-rate-stars">
+        <div
+          className="all-rate-stars flex
+        flex-col gap-3"
+        >
           <label className="flex items-center gap-2">
             <input
               type="checkbox"
-              className="w-5 h-5 border-gray-400 rounded focus:ring-black"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
             />
+            <SoldRating
+              className="justify-start"
+              soldText="800 Review"
+              ratingText="5"
+            />
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
+            />
+            <SoldRating
+              className="justify-start"
+              soldText="800 Review"
+              ratingText="5"
+            />
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
+            />
+            <SoldRating
+              className="justify-start"
+              soldText="800 Review"
+              ratingText="5"
+            />
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
+            />
+            <SoldRating
+              className="justify-start"
+              soldText="800 Review"
+              ratingText="5"
+            />
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
+            />
+            <SoldRating
+              className="justify-start"
+              soldText="800 Review"
+              ratingText="5"
+            />
+          </label>
+        </div>
+        <DashedLine className="!py-1" />
+        <div className="Rating">Review Topics</div>
+
+        <div
+          className="all-rate-stars flex
+        flex-col gap-3"
+        >
+          <label className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
+            />
+            <div className="Product-Quality">Product Quality</div>
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
+            />
+            <div className="Product-Quality">Seller Services</div>
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
+            />
+            <div className="Product-Quality">Product Price</div>
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
+            />
+            <div className="Product-Quality">Shipment</div>
+          </label>
+          <label className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              className="min-w-6 min-h-6 h-6 w-6 border-gray-400 rounded accent-black  focus:ring-black"
+            />
+            <div className="Product-Quality">Match with Description</div>
           </label>
         </div>
       </div>
